188139e13a12d171a9c2e1af52c5764bf0070450
Prakhar Agrawal <prakhargvp@gmail.com>
2018-07-03 09:26:41 +0530

    Adding React Router

--- src/app.js
+++ src/app.js
@@ -2,7 +2,43 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-ReactDOM.render(<p> This is my boilerplate </p>, document.getElementById('app'));
+const ExpensesDashboardPage = () => (
+	<div>
+		This is from my Dashboard Page.
+	</div>
+);
+
+const AddExpensesPage = () => (
+	<div>
+		This is my add expenses component.
+	</div>
+);
+
+const EditExpensesPage = () => (
+	<div>
+		This is my edit expenses component.
+	</div>
+);
+
+const HelpExpensesPage = () => (
+	<div>
+		This is my help expenses component.
+	</div>
+);
+
+const routes = (
+	<BrowserRouter>
+		<div>
+			<Route path="/"       component={ExpensesDashboardPage} exact={true} />
+			<Route path="/create" component={AddExpensesPage} />
+			<Route path="/edit"   component={EditExpensesPage} />
+			<Route path="/help"   component={HelpExpensesPage} />
+		</div>
+	</BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
