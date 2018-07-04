8f1ea5fe1e72e1111716d0b98cfd2fd8856d0b5c
Prakhar Agrawal <prakhargvp@gmail.com>
2018-07-04 17:23:31 +0530

    Playground: Array destructing , Object Destrutuing

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,34 +1,53 @@
-const person = {
-	name: 'Prakhar',
-	age: 21,
-	location: {
-		city: 'Haridwr',
-		temp: 23
-	}
-};
+// Object Destructuring
 
-const { name: firstName = 'Anonymous' , age } = person;
+// const person = {
+// 	name: 'Prakhar',
+// 	age: 21,
+// 	location: {
+// 		city: 'Haridwr',
+// 		temp: 23
+// 	}
+// };
 
-console.log(`${firstName} is ${age}`);
+// const { name: firstName = 'Anonymous' , age } = person;
 
-const { city, temp: temperature } = person.location;
+// console.log(`${firstName} is ${age}`);
 
-if( city && temperature){
-	console.log(`It's ${temperature} in ${city}.`);
-}
+// const { city, temp: temperature } = person.location;
 
-
-const book = {
-	title: 'Ego is the Enemy.',
-	author: 'Ryan Holiday',
-	publisher: {
-		name: 'Penguin'
-	}
-};
+// if( city && temperature){
+// 	console.log(`It's ${temperature} in ${city}.`);
+// }
 
 
-const { title, author } = book;
+// const book = {
+// 	title: 'Ego is the Enemy.',
+// 	author: 'Ryan Holiday',
+// 	publisher: {
+// 		name: 'Penguin'
+// 	}
+// };
 
-const { name: publisherName = 'Self Placed' } = book.publisher;
 
-console.log(publisherName);
+// const { title, author } = book;
+
+// const { name: publisherName = 'Self Placed' } = book.publisher;
+
+// console.log(publisherName);
+
+
+// Array Destructuring
+// 
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennysylvania', '19147'];
+
+const [, city, state = 'New York'] = address;
+
+console.log(`You are in ${city} ${state}.`);
+
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
